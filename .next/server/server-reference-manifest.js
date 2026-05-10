ea768d2711fbb6cccf2b03eed4d7dca32425e820
HIGO Developer <dev@higo.com>
2026-05-10 21:58:51 +0800

    chore: clean repository artifacts and update gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"6ncQVQoNDZ6MGZkb+enMzQnD2aE7AjvHzU3eoPaoqa0=\"}"
--- .next/static/chunks/app/products/page-3fa02201f4138fbf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5286,6833],{7089:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[5250,2971,8069,1744],function(){return n(n.s=7089)}),_N_E=n.O()}]);
